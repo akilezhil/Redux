adda2b43fc03b87c30c075b5e74d99e9e05a7812
Akilan <akilanms1999@gmail.com>
2026-03-31 17:57:06 +0530

    Added Redux setup and product features

---
+++ src/features/product/Product.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct } from "./productSlice";
+
+const Product = () => {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.product.products);
+
+  const handleAdd = () => {
+    if (name.trim() !== "") {
+      dispatch(addProduct(name));
+      setName("");
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Product App</h2>
+
+      <input
+        type="text"
+        placeholder="Enter product"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <button onClick={handleAdd}>Add Product</button>
+
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Product;
